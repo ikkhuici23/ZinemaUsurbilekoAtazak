66817b556fe09b2c15be388b13bf5878340ef0b9
Kerman Huici <ikkhuici23@lhusurbil.eus>
2025-11-18 09:31:13 +0100

    feat: comment code for javadoc

--- src/SoftwarearenGarapenaEskakizunOperazionalakProgramaInformatikoa.java
+++ src/SoftwarearenGarapenaEskakizunOperazionalakProgramaInformatikoa.java
@@ -1,23 +1,32 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 
 public class SoftwarearenGarapenaEskakizunOperazionalakProgramaInformatikoa {
-    public static final Scanner sc = new Scanner(System.in);
-
+    /** ANSI kolore gorria. */
     public static final String RED = "\u001b[31m";
+
+    /** ANSI kolore berdea. */
     public static final String GREEN = "\u001b[32m";
+
+    /** ANSI kolore horia. */
     public static final String YELLOW = "\u001b[33m";
+
+    /** ANSI reset-a. */
     public static final String RESET = "\u001b[0m";
 
+    /** Eskakizun operazionalak azalpeneko String ArrayList-a. */
     public static final ArrayList<String> eoAzalpenak = new ArrayList<>();
+
+    /** Programa informatikoa azalpeneko String ArrayList-a. */
     public static final ArrayList<String> piAzalpenak = new ArrayList<>();
 
+    // Azalpenak gehitu `eoAzalpenak`.
     static {
         eoAzalpenak.add("Eskakizun operazionalak sistema edo software baten funtzionamendu beharrak eta baldintzak zehazten dituzten dokumentuak dira.");
         eoAzalpenak.add("Helburua sistema edo softwareak bezeroaren beharrak modu eraginkorrean asetzea da.");
         eoAzalpenak.add("Eskakizun operazionalek funtzionalitate zehatzak, errendimendu baldintzak, segurtasun neurriak eta erabilgarritasun eskakizunak barne hartzen dituzte.");
     }
 
+    // Azalpenak gehitu `piAzalpenak`.
     static {
         piAzalpenak.add("Programa informatikoa software aplikazio edo sistemak dira, ordenagailuetan exekutatzeko diseinatuta daudenak.");
         piAzalpenak.add("Programa informatikoak hainbat helburu izan ditzakete, hala nola datuak prozesatzea, komunikazioa erraztea edo entretenimendua eskaintzea.");
@@ -26,7 +35,12 @@ public class SoftwarearenGarapenaEskakizunOperazionalakProgramaInformatikoa {
         piAzalpenak.add("Programa informatikoaren kalitatea erabiltzailearen esperientzian eta funtzionalitatean oinarritzen da.");
     }
 
+    /**
+     * Main metodoa.
+     * @param args komando lerroko argumentuak.
+     */
     public static void main(String[] args) {
+        // Eskakizun operazionaleko azalpen titulua.
         final String EO_TITULUA = "Zer dira eskakizun operazionalak?";
 
         System.out.println(GREEN + EO_TITULUA + RESET);
@@ -39,6 +53,7 @@ public class SoftwarearenGarapenaEskakizunOperazionalakProgramaInformatikoa {
 
         System.out.println();
 
+        // Programa informatikoko azalpen titulua.
         final String PI_TITULUA = "Zer da programa informatikoa?";
 
         System.out.println(GREEN + PI_TITULUA + RESET);
